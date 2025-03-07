a7b2050258ceaf8f2303ba3df7abf819f6581223
Roman Ivanov <sitdoff@gmail.com>
2025-03-08 00:06:13 +0300

    update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,3 +1,4 @@
-killall -q polybar                    # Отключение бара, если он включен
-echo "---" | tee -a /tmp/main_bar.log # Просмотр логов
-polybar main_bar >>/tmp/main_bar.log  # Запуск бара example и запись его лога
+killall -q polybar                       # Отключение бара, если он включен
+echo "---" | tee -a /tmp/main_bar.log    # Просмотр логов
+polybar main_bar >>/tmp/main_bar.log &   # Запуск бара example и запись его лога
+polybar second_bar >>/tmp/second_bar.log # Запуск бара example и запись его лога
